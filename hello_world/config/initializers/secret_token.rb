ca31f70d4cc5b2397adf47f905f592f35d001c4a
Neil Northrop <nnorthrop@gmail.com>
2012-12-28 02:56:16 -0800

    Portland Code School Application
    My first commit to my Portland Code School Application

---
+++ hello_world/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'ea35aee403bfe1ee97cf8ce46e195537e19f74c5bb9f509b86680011e592dc719be3a91eaaad71e119ad00b73ff73c6290a1faf2771b02e17fbc581e11bf4631'
